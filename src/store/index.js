b8e2c56f3e12e6db322ac8495444cbfba5f1ed6e
saran-shabd <shabd.179301181@muj.manipal.edu>
2019-07-23 03:27:07 +0530

    remove Redux Dev Tools from production

--- src/store/index.js
+++ src/store/index.js
@@ -9,9 +9,9 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
+    applyMiddleware(...middleware)
     // Redux DevTools
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
